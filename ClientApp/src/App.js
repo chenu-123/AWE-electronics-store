9e64196380981d181cc91a03a91a555c3ed14417
chenu123 <104697370@student.swin.edu.au>
2025-06-04 16:34:38 +1000

    modified:   ClientApp/src/App.js

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -3,6 +3,20 @@ import { Route, Routes } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 import { Layout } from './components/Layout';
 import './custom.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default class App extends Component {
   static displayName = App.name;
